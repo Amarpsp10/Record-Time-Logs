b315448bbcc594b110a4e726ca65ffa2c16abf27
amarpsp10 <amarpsp10@gmail.com>
2021-11-24 14:23:26 -0800

    server file updated

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var mysqlConnection = mysql.createConnection({
   user:'uhmrduzowsryr76c',
   password:'WKHTScJ1kUsbWzLs0IJk',
   database:'bz8qog4jpfmc0e7vgkdl',
+  multipleStatements: true
 })
 
 // Database Connection Check
@@ -22,11 +23,37 @@ mysqlConnection.connect((err)=>{
     console.log('DB connection failed /n Error : ' + JSON.stringify(err, undefined, 2));
 })
 
-//
+// Handling Routes
+
 app.get('/', (req, res) => {
   res.send('Hello World mere bai')
 })
 
+app.get('/time-logs', (req, res) =>{
+  mysqlConnection.query('SELECT * FROM Time_Logs', (err, rows, fields)=>{
+    if(!err){
+      res.status(200).send(rows);
+    }
+    else{
+      res.status(400).send('Something Went Wrong with Database');
+    }
+  })
+})
+
+app.delete('/time-logs/:id', (req,res) =>{
+  mysqlConnection.query('DELETE FROM Employee WHERE EmpID=?',[req.params.id],(err,rows,fields)=>{
+    if(!err){
+      res.status(200).send('Delete Successfully');
+    }
+    else{
+      res.status(400).send('Something Went wrong to delete');
+    }
+  })
+})
+
+app.post('/time-logs', (req,res)=>{
+  
+})
 // Listen
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
